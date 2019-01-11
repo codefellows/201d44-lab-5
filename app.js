727e731d7aafa2d3b7667110401964fb7eabcbad
abferris <abferris@gmail.com>
2019-01-11 14:29:51 -0800

    Update with addAndMultiply working

--- app.js
+++ app.js
@@ -52,11 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+   var resultSum = sum(sum(a,b)[0],c)[0];
+   var responseSum = a + ' and '+ b + ' and ' + c + ' sum to ' + resultSum +'.'
+  var resultMultiply= multiply(multiply(a,b)[0],c)[0];
+   var responseMultiply = 'The product of ' + a + ' and '+ b + ' and ' + c + ' is ' + resultMultiply + '.'
+return[resultSum, resultMultiply, responseSum, responseMultiply];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -79,7 +83,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ //testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
